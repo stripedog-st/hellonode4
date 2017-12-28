f3b1b2c153ecddd934c5610a879bb951da09ea9d
stripedog <33144844+stripedog-st@users.noreply.github.com>
2017-12-28 13:02:47 -0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('This is Nevins R&D Node 4.'))
+app.get('/', (req, res) => res.send('Allo! This is Node app #4.'))
 
 app.listen(8072, () => console.log('Example app listening on port 8072!'))
